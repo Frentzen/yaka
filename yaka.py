6316e70c729c9698b7231b2c38ab38f4bcb06c8f
frentzenamaral@gmail.com <frentzenamaral@gmail.com>
2019-06-05 15:27:14 -0300

    Colored

--- yaka.py
+++ yaka.py
@@ -113,13 +113,13 @@ print ""
 print banner
 
 if options.smb == True:
-    print (colored("--> Scanning files...", 'white', 'on_red', attrs=['bold']))
+    print (colored("--> Scanning for sensitive information in this shares:\n", 'white', 'on_red', attrs=['bold']))
     p1 = smbmodule.SMBModule(userID, password, server_name, server_ip, domain)
     conn, shares = p1.connlist()
     
     for ftx in shares:
 	    print "[*] Share "+ftx+" is avaliable"
-    
+    print ""
     if os == 'Win':
         smbmodule.smbtunner(userID, password, server_ip, shares, conn, '\\', pattern, Windows, temp, regexs, os)
     elif os == 'Lin':
